495d0d2048a9602fdba4eba48b31cc21790af154
Chris Withers <chris@simplistix.co.uk>
2013-06-30 16:23:03 +0100

    Boilerplate.

---
+++ README.txt
@@ -0,0 +1,15 @@
+====
+Mush
+====
+
+For full documentation please see:
+http://pythonhosted.org/mush
+
+If working offline, please consult the documentation source in the
+`docs` directory.
+
+Licensing
+=========
+
+Copyright (c) 2013 Simplistix Ltd
+See docs/license.txt for details.
---
+++ setup.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2013 Simplistix Ltd
+# See license.txt for license details.
+
+import os
+from ConfigParser import RawConfigParser
+from setuptools import setup, find_packages
+
+base_dir = os.path.dirname(__file__)
+
+# read test requirements from tox.ini
+config = RawConfigParser()
+config.read(os.path.join(base_dir, 'tox.ini'))
+test_requires = []
+for item in config.get('testenv', 'deps').split():
+    test_requires.append(item)
+# Tox doesn't need itself, but we need it for testing.
+test_requires.append('tox')
+
+setup(
+    name='mush',
+    version='1.0dev',
+    author='Chris Withers',
+    author_email='chris@simplistix.co.uk',
+    license='MIT',
+    description="Type-based dependency injection for scripts.",
+    long_description=open(os.path.join(base_dir,'docs','description.txt')).read(),
+    url='http://pypi.python.org/pypi/mush',
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    ],    
+    packages=find_packages(),
+    zip_safe=False,
+    extras_require=dict(
+        test=test_requires,
+        )
+    )
