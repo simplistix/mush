6ef923058dbaaec09e07d87434e83ac4d4d99ae4
Chris Withers <chris@simplistix.co.uk>
2013-09-09 08:03:09 +0100

    Get documentation tested again.

--- mush/tests/test_docs.py
+++ mush/tests/test_docs.py
@@ -4,10 +4,11 @@ from manuel import doctest, capture, codeblock
 from manuel.testing import TestSuite
 from os.path import dirname, join, pardir
 
-tests = glob(join(join(dirname(__file__), pardir), 'docs', '*.txt'))
+tests = glob(join(join(dirname(__file__), pardir, pardir), 'docs', '*.txt'))
 
 def test_suite():
     m =  doctest.Manuel(optionflags=REPORT_NDIFF|ELLIPSIS)
     m += codeblock.Manuel()
     m += capture.Manuel()
+    assert(len(tests) > 3)
     return TestSuite(m, *tests)
