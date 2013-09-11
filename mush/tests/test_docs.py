4196f99b253e9b7f2e7c7daab30764e3eaae1560
Chris Withers <chris@simplistix.co.uk>
2013-09-11 21:29:55 +0100

    oh, now I see... skip doc tests for tox, the docs won't be found

--- mush/tests/test_docs.py
+++ mush/tests/test_docs.py
@@ -2,17 +2,17 @@ from doctest import REPORT_NDIFF, ELLIPSIS
 from glob import glob
 from manuel import doctest, capture, codeblock
 from manuel.testing import TestSuite
+from nose.plugins.skip import SkipTest
 from os.path import dirname, join, pardir
 
-import os
+tests = glob(join(join(dirname(__file__), pardir, pardir), 'docs', '*.txt'))
 
-workspace = os.environ.get('WORKSPACE', join(dirname(__file__), pardir, pardir))
-path = join(workspace, 'docs', '*.txt')
-tests = glob(path)
-
+if not tests:
+    # tox can't find docs and installing an sdist doesn't install the docs
+    raise SkipTest('No docs found to test') # pragma: no cover
+    
 def test_suite():
     m =  doctest.Manuel(optionflags=REPORT_NDIFF|ELLIPSIS)
     m += codeblock.Manuel()
     m += capture.Manuel()
-    assert len(tests) > 3, repr((tests, path))
     return TestSuite(m, *tests)
