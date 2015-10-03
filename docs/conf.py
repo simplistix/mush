f575905ea817f8c00c4ece0805a51600a3ca60b1
Chris Withers <chris@simplistix.co.uk>
2015-10-03 11:39:41 -0500

    move to readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, pkginfo, datetime
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 pkg_info = pkginfo.Develop(os.path.join(os.path.dirname(__file__),'..'))
 
 extensions = [
@@ -19,14 +20,17 @@ master_doc = 'index'
 project = pkg_info.name
 first_year = 2013
 current_year = datetime.datetime.now().year
-copyright = (str(current_year) if current_year==first_year else ('%s-%s'%(first_year,current_year)))+' Simplistix Ltd'
+copyright = (str(current_year) if current_year==first_year else ('%s-%s'%(first_year,current_year)))+' Chris Withers'
 version = release = pkg_info.version
 exclude_trees = ['_build']
 unused_docs = ['description']
 pygments_style = 'sphinx'
 
 # Options for HTML output
-html_theme = 'default'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'classic'
 htmlhelp_basename = project+'doc'
 
 # Options for LaTeX output
