5268039446dbd99a942a2ea95dc11c4454f434ab
Chris Withers <chris@simplistix.co.uk>
2015-11-05 08:57:24 -0600

    Implement context.call, which now takes care of calling all objects.
    It takes an explicit requires and returns, meaning that Context itself can be further simplified.

--- mush/context.py
+++ mush/context.py
@@ -1,4 +1,7 @@
-from . import not_specified
+from collections import deque
+
+from .declarations import how, nothing
+from mush import missing
 
 NONE_TYPE = None.__class__
 
@@ -22,19 +25,40 @@ class Context(dict):
                     ))
         self[type] = it
 
-    def __getitem__(self, type):
-        """
-        Get an object of the specified type from the context.
-
-        This will raise a :class:`KeyError` if no object of that type
-        can be located.
-        """
-        if type is NONE_TYPE:
-            return None
-        obj = super(Context, self).get(type, not_specified)
-        if obj is not_specified:
-            raise KeyError('No %r in context' % type)
-        return obj
-
     def __repr__(self):
         return '<Context: %s>' % super(Context, self).__repr__()
+
+    def call(self, obj, requires, returns):
+
+        args = []
+        kw = {}
+
+        for name, type in requires:
+
+            ops = deque()
+            while isinstance(type, how):
+                ops.appendleft(type.process)
+                type = type.type
+
+            o = self.get(type, missing)
+
+            for op in ops:
+                o = op(o)
+                if o is nothing:
+                    break
+
+            if o is nothing:
+                pass
+            elif o is missing:
+                raise KeyError('No %r in context' % type)
+            elif name is None:
+                args.append(o)
+            else:
+                kw[name] = o
+
+        result = obj(*args, **kw)
+
+        for type, obj in returns.process(result):
+            self.add(obj, type)
+
+        return result
--- mush/tests/test_context.py
+++ mush/tests/test_context.py
@@ -1,10 +1,13 @@
 from unittest import TestCase
+from mock import Mock
 
-from testfixtures import ShouldRaise
+from testfixtures import ShouldRaise, compare
 
 from mush.context import Context
 
 from .compat import PY32
+from mush.declarations import nothing, result_type, requires, optional, item, \
+    attr, returns, returns_mapping
 
 
 class TheType(object):
@@ -69,11 +72,6 @@ class TestContext(TestCase):
         with ShouldRaise(ValueError('Context already contains '+repr(TheType))):
             context.add(obj2, TheType)
 
-    def test_missing(self):
-        context = Context()
-        with ShouldRaise(KeyError('No '+repr(TheType)+' in context')):
-            context[TheType]
-
     def test_clash_string_type(self):
         obj1 = TheType()
         obj2 = TheType()
@@ -81,11 +79,6 @@ class TestContext(TestCase):
         context.add(obj1, type='my label')
         with ShouldRaise(ValueError("Context already contains 'my label'")):
             context.add(obj2, type='my label')
-
-    def test_missing_string_type(self):
-        context = Context()
-        with ShouldRaise(KeyError("No 'my label' in context")):
-            context['my label']
 
     def test_add_none(self):
         context = Context()
@@ -97,6 +90,188 @@ class TestContext(TestCase):
         context.add(None, TheType)
         self.assertTrue(context[TheType] is None)
 
-    def test_get_none_type(self):
-        self.assertTrue(Context()[type(None)] is None)
+    def test_call_basic(self):
+        def foo():
+            return 'bar'
+        context = Context()
+        result = context.call(foo, nothing, result_type)
+        compare(result, 'bar')
+        compare({str: 'bar'}, context)
+
+    def test_call_requires_string(self):
+        def foo(obj):
+            return obj
+        context = Context()
+        context.add('bar', 'baz')
+        result = context.call(foo, requires('baz'), result_type)
+        compare(result, 'bar')
+        compare({'baz': 'bar', str: 'bar'}, context)
+
+    def test_call_requires_type(self):
+        def foo(obj):
+            return obj
+        context = Context()
+        context.add('bar', TheType)
+        result = context.call(foo, requires(TheType), result_type)
+        compare(result, 'bar')
+        compare({TheType: 'bar', str: 'bar'}, context)
+
+    def test_call_requires_missing(self):
+        def foo(obj):
+            return obj
+        context = Context()
+        with ShouldRaise(KeyError(
+                "No <class 'mush.tests.test_context.TheType'> in context"
+        )):
+            context.call(foo, requires(TheType), result_type)
+
+    def test_call_requires_named_parameter(self):
+        def foo(x, y):
+            return x, y
+        context = Context()
+        context.add('foo', TheType)
+        context.add('bar', 'baz')
+        result = context.call(foo,
+                              requires(y='baz', x=TheType),
+                              result_type)
+        compare(result, ('foo', 'bar'))
+        compare({TheType: 'foo',
+                 'baz': 'bar',
+                 tuple: ('foo', 'bar')}, context)
+
+    def test_call_requires_optional_present(self):
+        def foo(x=1):
+            return x
+        context = Context()
+        context.add(2, TheType)
+        result = context.call(foo,
+                              requires(optional(TheType)),
+                              result_type)
+        compare(result, 2)
+        compare({TheType: 2, int: 2}, context)
+
+    def test_call_requires_optional_missing(self):
+        def foo(x=1):
+            return x
+        context = Context()
+        result = context.call(foo,
+                              requires(optional(TheType)),
+                              result_type)
+        compare(result, 1)
+        compare({int: 1}, context)
+
+    def test_call_requires_optional_string(self):
+        def foo(x=1):
+            return x
+        context = Context()
+        context.add(2, 'foo')
+        result = context.call(foo,
+                              requires(optional('foo')),
+                              result_type)
+        compare(result, 2)
+        compare({'foo': 2, int: 2}, context)
+
+    def test_call_requires_item(self):
+        def foo(x):
+            return x
+        context = Context()
+        context.add(dict(bar='baz'), 'foo')
+        result = context.call(foo,
+                              requires(item('foo', 'bar')),
+                              result_type)
+        compare(result, 'baz')
+
+    def test_call_requires_attr(self):
+        def foo(x):
+            return x
+        m = Mock()
+        context = Context()
+        context.add(m, 'foo')
+        result = context.call(foo,
+                              requires(attr('foo', 'bar')),
+                              result_type)
+        compare(result, m.bar)
+
+    def test_call_requires_item_attr(self):
+        def foo(x):
+            return x
+        m = Mock()
+        m.bar= dict(baz='bob')
+        context = Context()
+        context.add(m, 'foo')
+        result = context.call(foo,
+                              requires(item(attr('foo', 'bar'), 'baz')),
+                              result_type)
+        compare(result, 'bob')
+
+    def test_call_requires_optional_item_missing(self):
+        def foo(x=1):
+            return x
+        context = Context()
+        result = context.call(foo,
+                              requires(optional(item('foo', 'bar'))),
+                              result_type)
+        compare(result, 1)
+
+    def test_call_requires_optional_item_present(self):
+        def foo(x=1):
+            return x
+        context = Context()
+        context.add(dict(bar='baz'), 'foo')
+        result = context.call(foo,
+                              requires(optional(item('foo', 'bar'))),
+                              result_type)
+        compare(result, 'baz')
+
+    def test_call_requires_item_optional_missing(self):
+        def foo(x=1):
+            return x
+        context = Context()
+        result = context.call(foo,
+                              requires(item(optional('foo'), 'bar')),
+                              result_type)
+        compare(result, 1)
+
+    def test_call_requires_item_optional_present(self):
+        def foo(x=1):
+            return x
+        context = Context()
+        context.add(dict(bar='baz'), 'foo')
+        result = context.call(foo,
+                              requires(item(optional('foo'), 'bar')),
+                              result_type)
+        compare(result, 'baz')
+
+    def test_returns_single(self):
+        def foo():
+            return 'bar'
+        context = Context()
+        result = context.call(foo, nothing, returns(TheType))
+        compare(result, 'bar')
+        compare({TheType: 'bar'}, context)
+
+    def test_returns_sequence(self):
+        def foo():
+            return 1, 2
+        context = Context()
+        result = context.call(foo, nothing, returns('foo', 'bar'))
+        compare(result, (1, 2))
+        compare({'foo': 1, 'bar': 2}, context)
+
+    def test_returns_mapping(self):
+        def foo():
+            return {'foo': 1, 'bar': 2}
+        context = Context()
+        result = context.call(foo, nothing, returns_mapping())
+        compare(result, {'foo': 1, 'bar': 2})
+        compare({'foo': 1, 'bar': 2}, context)
+
+    def test_ignore_return(self):
+        def foo():
+            return 'bar'
+        context = Context()
+        result = context.call(foo, nothing, returns())
+        compare(result, 'bar')
+        compare({}, context)
+
 
