0b3fbd6e739138caccfe66c38a92bc72019dc6b3
Chris Withers <chris@simplistix.co.uk>
2017-09-20 07:52:00 +0100

    Prepare for 2.7.2 release.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ base_dir = os.path.dirname(__file__)
 
 setup(
     name='mush',
-    version='2.7.1',
+    version='2.7.2',
     author='Chris Withers',
     author_email='chris@simplistix.co.uk',
     license='MIT',
