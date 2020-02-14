62e5c1433f1125ca5cae33df3f469ec0c878bd0d
Chris Withers <chris@withers.org>
2020-02-14 14:24:08 +0000

    prepare for 2.8.1 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ base_dir = os.path.dirname(__file__)
 
 setup(
     name='mush',
-    version='2.8.0',
+    version='2.8.1',
     author='Chris Withers',
     author_email='chris@simplistix.co.uk',
     license='MIT',
